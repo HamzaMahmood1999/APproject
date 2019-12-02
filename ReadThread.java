d43197c66f8ad7ef8ed656e1dedf4a5b4b7a916e
Sarah <58132791+sarahejaz@users.noreply.github.com>
2019-12-02 23:38:25 +0500

    Update and rename ClientThread2.java to ReadThread.java

--- ReadThread.java
+++ ReadThread.java
@@ -2,17 +2,16 @@ package client;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.*;
+import java.net.Socket;
 
-public class ClientThread2 implements Runnable {
+public class ReadThread implements Runnable {
 	private Socket client;
 	
-	public ClientThread2() {}
+	public ReadThread() {}
 	
-	public ClientThread2(Socket sentClient) {
+	public ReadThread(Socket sentClient) {
 		this.client=sentClient;
 	}
 	
